bf66ea08ed22ddd1937c0603de67d67934b1a4dc
Raúl <raulma_13@hotmail.com>
2024-04-09 10:41:50 +0200

    fix bug activo

--- resources/views/livewire/buscador.blade.php
+++ resources/views/livewire/buscador.blade.php
@@ -69,7 +69,7 @@
                                                         <span class="h-1.5 w-1.5 rounded-full bg-gray-900"></span>
                                                         Ya no es socio
                                                     </span>
-                                                @elseif($mantenimiento)    
+                                                @elseif($activo)    
                                                     <span class="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
                                                         <span class="h-1.5 w-1.5 rounded-full bg-green-600"></span>
                                                         Cuota pagada
