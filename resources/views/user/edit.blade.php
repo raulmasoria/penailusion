c868d5c95b0be320656a43245c092e7f7647bcf7
Raúl <raulma_13@hotmail.com>
2025-02-26 20:31:28 +0100

    Order users profile and href godfather

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.plantilla')
 
-@section('contenido')   
-    
+@section('contenido')
+
     <div class="py-12">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 space-y-6">
             @if (session('status') === 'user-create')
@@ -24,10 +24,8 @@
             @endif
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('user.partials.update-profile-information-form')
-            </div>            
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                @include('user.partials.update-intolerances-form')
             </div>
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('user.partials.view-year-antiquity')
             </div>
@@ -35,6 +33,11 @@
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('user.partials.view-godfathers')
             </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                @include('user.partials.update-intolerances-form')
+            </div>
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('user.partials.update-adress-form')
             </div>
--- resources/views/user/partials/view-godfathers.blade.php
+++ resources/views/user/partials/view-godfathers.blade.php
@@ -7,72 +7,72 @@
         <p class="mt-1 text-sm text-gray-600">
             {{ __("Aquí puedes consultar los padrinos que ha tenido este peñista para entrar en la asociación.") }}
         </p>
-        
+
     </header>
-     
-    @if ($godfathers)    
+
+    @if ($godfathers)
         <table class="w-full border-collapse bg-white text-center text-gray-500 mt-6">
             <thead class="bg-orange-500">
                 <tr>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Padrino 1</th>                
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Padrino 1</th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Padrino 2</th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Año apadrinamiento</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-                
-                    <tr class="hover:bg-gray-50">               
-                        <th class="flex gap-3 px-6 py-4 font-normal text-gray-900 text-center">                    
-                            {{ $godfather1->name . ' ' . $godfather1->lastname}}            
+
+                    <tr class="hover:bg-gray-50">
+                        <th class="flex gap-3 px-6 py-4 font-normal text-gray-900 text-center">
+                            <a href="/socios/{{ $godfather1->id }}">{{ $godfather1->name . ' ' . $godfather1->lastname}}</a>
                         </th>
-                        <td class="px-6 py-4 font-normal text-gray-900 text-center">                    
-                            {{ $godfather2->name . ' ' . $godfather2->lastname }}         
+                        <td class="px-6 py-4 font-normal text-gray-900 text-center">
+                            <a href="/socios/{{ $godfather2->id }}">{{ $godfather2->name . ' ' . $godfather2->lastname }}</a>
                         </td>
-                        <td class="px-6 py-4 font-normal text-gray-900 text-center">                    
-                            {{ $godfathers->year_godfather }}         
+                        <td class="px-6 py-4 font-normal text-gray-900 text-center">
+                            {{ $godfathers->year_godfather }}
                         </td>
-                    </tr> 
+                    </tr>
             </tbody>
-        </table>    
+        </table>
     @else
         <div class="mt-6">
             <h4>Este peñista no tiene padrinos</h4>
         </div>
-    @endif   
+    @endif
 
     <hr class="mt-6">
-    
-    <header class="mt-6">        
+
+    <header class="mt-6">
         <p class="mt-1 text-sm text-gray-600">
             {{ __("Aquí puedes consultar a quién ha apadrinado este peñista.") }}
-        </p>        
+        </p>
     </header>
 
-    @if ($all_godfather)    
+    @if ($all_godfather)
         <table class="w-full border-collapse bg-white text-center text-gray-500 mt-6">
             <thead class="bg-orange-500">
                 <tr>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Apadrinó a</th>             
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Apadrinó a</th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Año apadrinamiento</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-100 border-t border-gray-100">
                 @foreach ($all_godfather as $year => $data_godfather)
-                    <tr class="hover:bg-gray-50">               
-                        <th class="flex gap-3 px-6 py-4 font-normal text-gray-900 text-center">                    
-                            {{ $data_godfather->name . ' ' . $data_godfather->lastname}}            
+                    <tr class="hover:bg-gray-50">
+                        <th class="flex gap-3 px-6 py-4 font-normal text-gray-900 text-center">
+                            {{ $data_godfather->name . ' ' . $data_godfather->lastname}}
                         </th>
-                        <td class="px-6 py-4 font-normal text-gray-900 text-center">                    
-                            {{ $year }}         
+                        <td class="px-6 py-4 font-normal text-gray-900 text-center">
+                            {{ $year }}
                         </td>
-                    </tr> 
-                @endforeach  
+                    </tr>
+                @endforeach
             </tbody>
-        </table>    
+        </table>
     @else
         <div class="mt-6">
             <h4>Este peñista no ha apadrinado a nadie todavia.</h4>
         </div>
-    @endif   
-    
+    @endif
+
 </section>
