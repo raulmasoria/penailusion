52d5db10d2ab7d20754ff86977e7fac6fcc70982
Raúl <raulma_13@hotmail.com>
2025-10-14 11:30:19 +0200

    Footer email

--- resources/views/mail/plantillaEmail.blade.php
+++ resources/views/mail/plantillaEmail.blade.php
@@ -54,6 +54,11 @@
                 Canal WhatsApp: <a href="https://whatsapp.com/channel/0029VafM2Fw1noz1A0zYL233" target="_blank">https://whatsapp.com/channel/0029VafM2Fw1noz1A0zYL233</a>
             </span>
         </p>
+        <p>
+            <span class="text-footer-light">
+                En cumplimiento de lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 y en la Ley Orgánica 3/2018, de 5 de diciembre, de Protección de Datos Personales y garantía de los derechos digitales, le informamos que sus datos personales son tratados por la Asociación Cultural Peña Ilusión. Podrá revocar su consentimiento para el tratamiento de sus datos personales, así como ejercitar los derechos de acceso, rectificación, supresión y oposición, limitación del tratamiento, portabilidad de datos, enviando un correo electrónico a la dirección <a href="mailto:soriailusion@gmail.com">soriailusion@gmail.com</a> con el asunto “PROTECCIÓN DE DATOS”. Para más información consulte nuestra política de privacidad en el siguiente enlace: <a href="https://soriailusion.com/wp-content/uploads/2025/05/Politica-de-privacidad.pdf" target="_blank">Política de Privacidad</a>.
+            </span>
+        </p>
     </div>
 </body>
 
